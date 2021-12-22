d9dddd7e412a9a1df7984cd1b3c6e66e21ea94e7
Y3drk <ziebura.jedrzej@gmail.com>
2021-12-22 11:33:29 +0100

    Saving Data to CSV added, functionalities completed, code structuring left

--- src/main/java/agh/ics/project/gui/App.java
+++ src/main/java/agh/ics/project/gui/App.java
@@ -16,10 +16,13 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class App extends Application {
@@ -89,7 +92,6 @@ public class App extends Application {
     protected Alert magicTP;
     protected Alert magicWL;
 
-    //---tracking & toggle buttons
     protected ToggleGroup togglesTP = new ToggleGroup();
     protected ToggleGroup togglesWL = new ToggleGroup();
 
@@ -104,7 +106,21 @@ public class App extends Application {
     protected Label currentGenomeWL;
     protected int currentDescendantsWL = 0;
     protected VBox trackedAnimalInfoWL;
-    //-------------
+
+    protected List<String[]> csvDataTP = new ArrayList<>();
+    protected int sumAliveAnimalsTP = 0;
+    protected int sumPresentGrassTP = 0;
+    protected int sumAverageEnergyTP = 0;
+    protected int sumAverageChildrenTP = 0;
+    protected int sumAverageLifeLengthTP = 0;
+
+    protected List<String[]> csvDataWL = new ArrayList<>();
+    protected int sumAliveAnimalsWL = 0;
+    protected int sumPresentGrassWL = 0;
+    protected int sumAverageEnergyWL = 0;
+    protected int sumAverageChildrenWL = 0;
+    protected int sumAverageLifeLengthWL = 0;
+
 
     public void init() {
         try {
@@ -139,6 +155,10 @@ public class App extends Application {
         } catch (FileNotFoundException ex){
             System.out.println("FILE NOT FOUND");
         }
+
+        String[] header = {"day","living animals","present grass", "average energy","average children","average life lenght"};
+        csvDataTP.add(header);
+        csvDataWL.add(header);
     }
 
     @Override
@@ -338,6 +358,16 @@ public class App extends Application {
             }
         });
 
+        saveStatsTP.setOnAction(event -> {
+            if(ifTeleportMapStopped){
+                try {
+                    saveDataToFile(engine,true);
+                } catch (IOException e) {
+                    System.out.println("IO STREAM EXCEPTION");
+                }
+            }
+        });
+
         startStopButtonWL.setOnAction(event -> {
             if(ifWalledMapStopped){
                 this.walledEngineThread = new Thread(walledEngine);
@@ -371,6 +401,17 @@ public class App extends Application {
                     setToggles(walledBoard,walledMap, false, walledEngine);
 
                     genomeHighlightedWL = false;
+                }
+            }
+        });
+
+        saveStatsWL.setOnAction(event -> {
+            if(ifWalledMapStopped){
+                try {
+                    saveDataToFile(walledEngine,false);
+                }
+                catch (IOException e) {
+                    System.out.println("IO STREAM EXCEPTION");
                 }
             }
         });
@@ -472,6 +513,12 @@ public class App extends Application {
             this.averageEnergyWL.getData().add(new XYChart.Data<>(engine.getDay(),map.getAverageEnergy()));
             this.averageChildrenAmountWL.getData().add(new XYChart.Data<>(engine.getDay(), map.getAverageChildren()));
             this.averageDaysLivedWL.getData().add(new XYChart.Data<>(engine.getDay(), engine.getAverageLifeLength()));
+        }
+
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException ex){
+            System.out.println("THREAD WAS INTERRUPTED");
         }
     }
 
@@ -704,6 +751,10 @@ public class App extends Application {
                         updateDominant(map);
                         grid.setGridLinesVisible(true);
 
+                        //-----------
+                        addData(engine, map, map.getTeleportValue());
+                        //---------saving data to csv connected stuff
+
                         if (map.getTeleportValue() && trackedTP != null){
                             setTrackingInfo(trackedTP,map,engine);
                         }
@@ -728,6 +779,91 @@ public class App extends Application {
         thread.start();
     }
 
+    public void addData(SimulationEngine engine, IWorldMap map, boolean whichMap){
+        int dayNumber = engine.getDay();
+        int animalsToday = map.countAnimals();
+        int grassToday = map.countGrass();
+        int averageEnergyToday = map.getAverageEnergy();
+        int averageChildrenToday = map.getAverageChildren();
+        int averageLifeLengthToday = engine.getAverageLifeLength();
+        String[] newRecord = {Integer.toString(dayNumber),Integer.toString(animalsToday),Integer.toString(grassToday), Integer.toString(averageEnergyToday), Integer.toString(averageChildrenToday), Integer.toString(averageLifeLengthToday)};
+        if (whichMap){
+            csvDataTP.add(newRecord);
+            sumAliveAnimalsTP += animalsToday;
+            sumPresentGrassTP += grassToday;
+            sumAverageEnergyTP += averageEnergyToday;
+            sumAverageChildrenTP += averageChildrenToday;
+            sumAverageLifeLengthTP += averageLifeLengthToday;
+        } else {
+            csvDataWL.add(newRecord);
+            sumAliveAnimalsWL += animalsToday;
+            sumPresentGrassWL += grassToday;
+            sumAverageEnergyWL += averageEnergyToday;
+            sumAverageChildrenWL += averageChildrenToday;
+            sumAverageLifeLengthWL += averageLifeLengthToday;
+        }
+    }
+
+    public void saveDataToFile(SimulationEngine engine, boolean whichMap) throws IOException{
+        int dayNumber = engine.getDay();
+        //TO FINISH
+        if (whichMap){
+            int animalsTotal = sumAliveAnimalsTP/dayNumber;
+            int grassTotal = sumPresentGrassTP/dayNumber;
+            int averageEnergyTotal = sumAverageEnergyTP/dayNumber;
+            int averageChildrenTotal = sumAverageChildrenTP/dayNumber;
+            int averageLifeLengthTotal = sumAverageLifeLengthTP/dayNumber;
+
+            String[] newRecord = {"Summary",Integer.toString(animalsTotal),Integer.toString(grassTotal), Integer.toString(averageEnergyTotal), Integer.toString(averageChildrenTotal), Integer.toString(averageLifeLengthTotal)};
+            csvDataTP.add(newRecord);
+
+            //System.out.println(csvDataTP.size());
+            File statisticsTP = new File("statisticsTP.csv");
+            try (PrintWriter pwTP = new PrintWriter(statisticsTP)){
+                csvDataTP.stream()
+                        .map(this::convertToCSV)
+                        .forEach(pwTP::println);
+            } catch (IOException e){
+                System.out.println("IO STREAM EXCEPTION");
+            }
+
+        } else {
+            int animalsTotal = sumAliveAnimalsWL/dayNumber;
+            int grassTotal = sumPresentGrassWL/dayNumber;
+            int averageEnergyTotal = sumAverageEnergyWL/dayNumber;
+            int averageChildrenTotal = sumAverageChildrenWL/dayNumber;
+            int averageLifeLengthTotal = sumAverageLifeLengthWL/dayNumber;
+
+            String[] newRecord = {"Summary",Integer.toString(animalsTotal),Integer.toString(grassTotal), Integer.toString(averageEnergyTotal), Integer.toString(averageChildrenTotal), Integer.toString(averageLifeLengthTotal)};
+            csvDataWL.add(newRecord);
+
+            //System.out.println(csvDataWL);
+            File statisticsWL = new File("statisticsWL.csv");
+            try (PrintWriter pwWL = new PrintWriter(statisticsWL)){
+                csvDataWL.stream()
+                        .map(this::convertToCSV)
+                        .forEach(pwWL::println);
+            } catch (IOException e){
+                System.out.println("IO STREAM EXCEPTION");
+            }
+        }
+    }
+
+    public String convertToCSV(String[] data) {
+        return Stream.of(data)
+                .map(this::escapeSpecialCharacters)
+                .collect(Collectors.joining(","));
+    }
+
+    public String escapeSpecialCharacters(String data) {
+        String escapedData = data.replaceAll("\\R", " ");
+        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+            data = data.replace("\"", "\"\"");
+            escapedData = "\"" + data + "\"";
+        }
+        return escapedData;
+    }
+
     @Override
     public void stop(){
         System.out.println("THE VISUALIZATION WINDOW WAS CLOSED");
@@ -741,4 +877,5 @@ public class App extends Application {
         grid.getChildren().clear();
     }
 }
+
 
